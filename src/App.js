477b116bd0e2135c1428777068ce51ef1bfd4fe8
Owen Horner <owenhorner@live.com.au>
2019-01-29 09:25:56 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var classNames = require('classnames');
+
+class Box extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: false }
+    this.handleOnMouse = this.handleOnMouse.bind(this);
+  }
+
+  handleOnMouse() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+  
+  render() {
+    var liClasses = classNames({
+      'RedSquare': !this.state.isToggleOn,
+      'BlackSquare': this.state.isToggleOn
+    });
+
+    return (
+      <div className={liClasses} onMouseEnter={this.handleOnMouse} onMouseLeave={this.handleOnMouse}></div>
+    )
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { boxes: [] };
+  }
+
+  componentDidMount() {
+    let boxes = []
+    for (let i = 0; i < 2000; ++i) {
+      boxes.push(<Box />)
+    }
+    this.setState({ boxes: boxes })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.boxes}
       </div>
     );
   }
